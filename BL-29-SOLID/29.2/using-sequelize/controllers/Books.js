c0fc8cc6d5574e6aa1eada26fbb0e2be07a99c55
joaoAndradeJr <fullservicemanaus@gmail.com>
2021-10-11 10:18:57 -0400

    add try catch block

--- BL-29-SOLID/29.2/using-sequelize/controllers/Books.js
+++ BL-29-SOLID/29.2/using-sequelize/controllers/Books.js
@@ -1,34 +1,56 @@
 const { Books } = require('../models');
 
 const getall = async (_req, res) => {
-  const books = await Books.findAll();
-  res.status(200).json(books);
+  try {
+    const books = await Books.findAll();
+    res.status(200).json(books);
+  } catch (error) {
+    return res.status(500).json({ message: 'Algo deu errado' });
+  }
 }
 
 const getById = async (req, res) => {
-  const { id } = req.params;
-  const book = await Books.findByPk(id);
-  res.status(200).json(book);
+  try {
+    const { id } = req.params;
+    const book = await Books.findByPk(id);
+    res.status(200).json(book);
+  } catch (error) {
+    return res.status(500).json({ message: 'Algo deu errado' });
+  } 
 };
 
 const create = async (req, res) => {
-  const { title, author, pageQuantity = 0, createdAt = new Date() } = req.body;
-  const book = await Books.create({ title, author, pageQuantity, createdAt });
-  res.status(201).json(book);
+  try {
+    const { title, author, pageQuantity = 0, createdAt = new Date() } = req.body;
+    const book = await Books.create({ title, author, pageQuantity, createdAt });
+    res.status(201).json(book);
+  } catch (error) {
+    return res.status(500).json({ message: 'Algo deu errado' });
+  }
 };
 
 const update = async (req, res) => {
-  const { id } = req.params;
-  const { title, author, pageQuantity = 0, createdAt = new Date() } = req.body;
-  const updated = await Books.update({ title, author, pageQuantity, createdAt }, { where: { id } });
-  res.status(201).json(updated);
+  try {
+    const { id } = req.params;
+    const { title, author, pageQuantity = 0, createdAt = new Date() } = req.body;
+    const updated = await Books.update(
+      { title, author, pageQuantity, createdAt }, { where: { id } },
+    );
+    res.status(201).json(updated);
+  } catch (error) {
+    return res.status(500).json({ message: 'Algo deu errado' });
+  }
 };
 
 const remove = async (req, res) => {
-  const { id } = req.params;
-  const book = await Books.findByPk(id);
-  await book.destroy();
-  res.status(200).json(book);
+  try {
+    const { id } = req.params;
+    const book = await Books.findByPk(id);
+    await book.destroy();
+    res.status(200).json(book);
+  } catch (error) {
+    return res.status(500).json({ message: 'Algo deu errado' });
+  }
 }
 
 module.exports = {
