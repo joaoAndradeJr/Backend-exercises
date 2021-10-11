ff7db8093426f23bd86adf0713c84e12bb756070
joaoAndradeJr <fullservicemanaus@gmail.com>
2021-10-11 11:13:04 -0400

    add base sequelize and migrations and models

---
+++ BL-29-SOLID/29.3/migrations/20211011143534-create-employees.js
@@ -0,0 +1,32 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    return queryInterface.createTable('Employees', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      firstName: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+        field: 'first_name',
+      },
+      lastName: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+        field: 'last_name',
+      },
+      age: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+      },
+    });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Employees');
+  }
+};
